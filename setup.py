f531ab549b3ba0df92df0a49bb5335c6d4f0b69a
Isaac Campbell <isaac.campbell@dtc.ox.ac.uk>
2025-07-02 15:09:20 +0100

    Add setup.py file

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="2s2r",
+    version="0.1.0",
+    description="MARL environment for Two Species, Two Resources (2S2R) problem",
+    author="Isaac Campbell",
+    author_email="isaac.campbell@wolfson.ox.ac.uk",
+    packages=find_packages(),
+    install_requires=[
+        "ipykernel>=6.29.5",
+        "jax>=0.6.2",
+        "jax-dataclasses>=1.6.2",
+        "tensorflow-probability>=0.25.0",
+    ],
+    python_requires=">=3.12",
+    url="https://github.com/isaachcamp/marlplay.git",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GPL-3.0 License",
+    ],
+)
