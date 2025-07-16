9d18a3e8836f992e611249696497f27bf9465f26
Isaac Campbell <isaac.campbell@dtc.ox.ac.uk>
2025-07-16 14:29:05 +0100

    test geometry is jittable

--- test/test_geometry.py
+++ test/test_geometry.py
@@ -1,6 +1,7 @@
 
 from twoStwoR.geometry import area_of_intersecting_circles
 import jax.numpy as jnp
+import jax
 
 def test_area_of_intersecting_circles_no_intersection():
     xy1 = jnp.array([0.0, 0.0])
@@ -25,3 +26,6 @@ def test_area_of_intersecting_circles_partial_intersection():
     r2 = jnp.array(2.0)
     area = area_of_intersecting_circles(xy1, r1, xy2, r2)
     assert 0 < area < jnp.pi * r1**2
+
+def test_area_of_intersecting_circles_jittable():
+    assert jax.jit(area_of_intersecting_circles)
