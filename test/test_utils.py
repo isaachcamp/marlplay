0fd6e7017e6cc72e81fb8e146df99ed1752e152f
Isaac Campbell <isaac.campbell@dtc.ox.ac.uk>
2025-08-05 10:49:48 +0100

    Update util fn to align with prepping inside step_env

--- test/test_utils.py
+++ test/test_utils.py
@@ -3,35 +3,16 @@ from typing import Dict
 import jax
 
 
-def gen_random_actions(key) -> Dict[str, Dict[str, jax.Array]]:
+def gen_random_actions(key) -> Dict[str, jax.Array]:
     """
     Generates random actions for both Tree and Fungus agents.
     Each action is a float in the range [0.0, 1.0].
     
-    {'tree': {'p_use': float, ' p_trade': float, 's_use': float, 's_trade': float, 
-              'growth': float, 'defence': float, 'reproduction': float},
-    'fungus': {'p_use': float, ' p_trade': float, 's_use': float, 's_trade': float, 
-               'growth': float, 'defence': float, 'reproduction': float}}
+    {'tree': jnp.array(7,), 'fungus': jnp.array(7,)}
     """
     key, tree_key, fungus_key = jax.random.split(key, 3)
 
-    tree_actions = {
-        'p_use': jax.random.uniform(tree_key, shape=(), minval=0.0, maxval=1.0),
-        'p_trade': jax.random.uniform(tree_key, shape=(), minval=0.0, maxval=1.0),
-        's_use': jax.random.uniform(tree_key, shape=(), minval=0.0, maxval=1.0),
-        's_trade': jax.random.uniform(tree_key, shape=(), minval=0.0, maxval=1.0),
-        'growth': jax.random.uniform(tree_key, shape=(), minval=0.0, maxval=1.0),
-        'defence': jax.random.uniform(tree_key, shape=(), minval=0.0, maxval=1.0),
-        'reproduction': jax.random.uniform(tree_key, shape=(), minval=0.0, maxval=1.0)
-    }
-    fungus_actions = {
-        'p_use': jax.random.uniform(fungus_key, shape=(), minval=0.0, maxval=1.0),
-        'p_trade': jax.random.uniform(fungus_key, shape=(), minval=0.0, maxval=1.0),
-        's_use': jax.random.uniform(fungus_key, shape=(), minval=0.0, maxval=1.0),
-        's_trade': jax.random.uniform(fungus_key, shape=(), minval=0.0, maxval=1.0),
-        'growth': jax.random.uniform(fungus_key, shape=(), minval=0.0, maxval=1.0),
-        'defence': jax.random.uniform(fungus_key, shape=(), minval=0.0, maxval=1.0),
-        'reproduction': jax.random.uniform(fungus_key, shape=(), minval=0.0, maxval=1.0)
-    }
+    tree_actions = jax.random.uniform(tree_key, shape=(7,), minval=0.0, maxval=1.0)
+    fungus_actions = jax.random.uniform(fungus_key, shape=(7,), minval=0.0, maxval=1.0)
 
     return {'tree': tree_actions, 'fungus': fungus_actions}
